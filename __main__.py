5d444897a2ec4c5c5e8b4d030f454f3ebc5b38d9
Mitchell <m.steinberg42@gmail.com>
2020-09-22 16:40:22 -0700

    Laid out initial classes

---
+++ __main__.py
@@ -0,0 +1,15 @@
+class BulletJournal:
+
+
+    def __init__(self):
+
+    def collection(self):
+        self.title
+        self.page_number
+        self.divisions
+
+    def daily_log(self):
+
+    def future_log(self):
+
+    def monthly_log(self):
