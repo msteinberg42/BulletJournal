7a54630cf5125bc27aa400fcd7e7a38eedf5dcd4
Mitchell <m.steinberg42@gmail.com>
2020-09-29 12:49:21 -0700

    Switched to urwid and poetry

--- __main__.py
+++ __main__.py
@@ -1,14 +1,32 @@
-class Collection:
+import urwid
 
-    def __init__(self):
-        self.title
-        self.page_number
-        self.divisions
+def exit_on_q(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
 
-    def daily_log(self):
+palette = [
+        ('banner', 'black', 'light gray'),
+        ('streak', 'black', 'dark red'),
+        ('bg', 'black', 'dark blue'),]
 
-    def future_log(self):
+txt = urwid.Text(('banner', u"Hello World"), align='center')
+map1 = urwid.AttrMap(txt, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
+loop.run()
 
-    def monthly_log(self):
+# class Collection:
 
-    def index(self):
+    # def __init__(self):
+        # self.title
+        # self.page_number
+        # self.divisions
+
+    # def daily_log(self):
+
+    # def future_log(self):
+ 
+    # def monthly_log(self):
+
+    # def index(self):
--- test.py
+++ test.py
@@ -4,14 +4,29 @@ import curses
 def main(stdscr):
     curses.curs_set(0)
     stdscr.refresh()
-    show_list(stdscr)
+    numbers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    # numbers = ['a', 'b', 'c'] 
+    begin_x = 20; begin_y = 7
+    height = 5; width = 40
+    curwin = curses.newwin(height, width, begin_y, begin_x)
+    curwin.box(0, 0)
+    show_list(curwin, numbers, 0)
+    scroll_list(curwin, numbers, 1)
+    scroll_list(curwin, numbers, 2)
 
-def show_list(stdscr):
-    numbers = ['a', 'b', 'c']
-    for i in range(len(numbers)):
-        stdscr.addstr(i, 5, numbers[i-1]) 
-        stdscr.refresh()
-    time.sleep(5)
+def show_list(curwin, entries, pos):
+    for i in range(curwin.getmaxyx()[0] - 2):
+        try: 
+            curwin.addstr(i+1+pos, 5, entries[i]) 
+        except:
+            print('FAILED')
+        curwin.refresh()
+    time.sleep(2)
+
+def scroll_list(curwin, entries, pos):
+    curwin.clear()
+    pos += 1
+    show_list(curwin, entries, pos)
 
 curses.wrapper(main)
 
